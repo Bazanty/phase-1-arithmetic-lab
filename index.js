4c154f643e0e683238ad5164246934e45506e6cb
bazanty <bazantyloffan@gmail.com>
2024-09-25 20:35:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+// num1
+// num2
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+// Write your code here on
+const random = Math.floor(Math.random() * 100) + 1;
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4;
+// 10 % 6 = 4
+const max = Math.max(1, 2, 4, 18, 20);
+ 
